e00f084b8f8e520eae3b6926862660cbbe037fd7
Genes Luna <genesluna@gmail.com>
2023-05-14 17:29:08 -0300

    added colors

---
+++ colors.js
@@ -0,0 +1,61 @@
+module.exports = {
+  transparent: "transparent",
+
+  primary: {
+    DEFAULT: "#f28c18",
+    focus: "#c9710d",
+    faded: "#f28c182a",
+  },
+
+  secondary: {
+    DEFAULT: "#187ef2",
+    focus: "#0d65c9",
+    faded: "#187ef22a",
+  },
+
+  accent: {
+    DEFAULT: "#4fa300",
+    focus: "#367000",
+  },
+
+  danger: {
+    DEFAULT: "#ff6347",
+    focus: "#DB4437",
+  },
+
+  neutral: {
+    DEFAULT: "#1b1d1d",
+    focus: "#131616",
+  },
+
+  content: {
+    100: "#ffffff",
+    150: "#F0F0F0",
+    200: "#cbd5e1",
+    300: "#64748b",
+    400: "#131616",
+  },
+
+  base: {
+    50: "#ffffff",
+    100: "#F0F0F0",
+    150: "#e7e9eb",
+    200: "#e2e8f0",
+    300: "#cbd5e1",
+    350: "#64748b",
+    400: "#2f2f2f",
+    450: "#2b2b2b",
+    500: "#212121",
+    550: "#1b1d1d",
+    600: "#181818",
+    700: "#0d0d0d",
+  },
+
+  info: "#66c7ff",
+  success: "#87cf3a",
+  warning: "#f7c250",
+  error: {
+    DEFAULT: "#ff6b6b",
+    focus: "#dc2626",
+  },
+};
