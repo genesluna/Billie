87724495c00810c9f9ba71692a6d500ac835f383
Genes Luna <genesluna@gmail.com>
2023-05-16 00:31:09 -0300

    removed useless safearea

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
+import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, useColorScheme } from "react-native";
-import Home from "./src/screens/Home";
+import { Routes } from "./src/routes";
 
 export default function App() {
   let colorScheme = useColorScheme();
 
   return (
-    <SafeAreaView className="flex-1">
-      <Home />
+    <>
+      <Routes />
       <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-    </SafeAreaView>
+    </>
   );
 }
