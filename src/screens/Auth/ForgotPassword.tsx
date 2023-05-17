b97546f9ef39fb3549778edc8ef48dbd076e2c80
Genes Luna <genesluna@gmail.com>
2023-05-17 12:59:12 -0300

    added forgot password screen

---
+++ src/screens/Auth/ForgotPassword.tsx
@@ -0,0 +1,34 @@
+import { Keyboard, KeyboardAvoidingView, Platform, ToastAndroid, TouchableWithoutFeedback } from "react-native";
+
+import { useAuth } from "../../context/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import Container from "../../components/common/Container";
+import ForgotPasswordForm, { ForgotPaswordFormValues } from "../../components/forms/Auth/ForgotPasswordForm";
+
+const ForgotPassword = () => {
+  const navigation = useNavigation();
+  const { resetPassword } = useAuth();
+
+  async function handleResetPassword({ email }: ForgotPaswordFormValues): Promise<void> {
+    try {
+      await resetPassword(email);
+      ToastAndroid.show("E-mail enviado com sucesso.", ToastAndroid.LONG);
+      navigation.goBack();
+    } catch (error) {
+      ToastAndroid.show("Email inválido", ToastAndroid.LONG);
+      console.log(error);
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView className="flex-1" behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container className="px-6">
+          <ForgotPasswordForm onSubmit={handleResetPassword} />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default ForgotPassword;
