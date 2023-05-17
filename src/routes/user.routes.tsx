a59a758b896684d5728922c22d588ae69d271af3
Genes Luna <genesluna@gmail.com>
2023-05-17 13:01:20 -0300

    added add transaction and report tabs

--- src/routes/user.routes.tsx
+++ src/routes/user.routes.tsx
@@ -1,9 +1,12 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useColorScheme } from "react-native";
+import { Feather as Icon } from "@expo/vector-icons";
 
-import Home from "../screens/user/Home";
+import Home from "../screens/User/Home";
 import colors from "../../colors";
-const { Screen, Navigator } = createNativeStackNavigator();
+import AddTransaction from "../screens/User/AddTransaction";
+import Reports from "../screens/User/Reports";
+const { Screen, Navigator } = createBottomTabNavigator();
 
 export function UserRoutes() {
   let colorScheme = useColorScheme();
@@ -15,17 +18,45 @@ export function UserRoutes() {
         headerTitleAlign: "center",
         headerTintColor: colors.content[150],
         headerStyle: { backgroundColor: colors.primary.DEFAULT },
-        animation: "slide_from_right",
-        contentStyle: { backgroundColor: colorScheme === "dark" ? colors.base[600] : colors.base[150] },
+        tabBarInactiveBackgroundColor: colorScheme === "dark" ? colors.base[600] : colors.primary.DEFAULT,
+        tabBarActiveBackgroundColor: colorScheme === "dark" ? colors.base[500] : colors.primary.focus,
+        tabBarActiveTintColor: colors.content[150],
+        tabBarInactiveTintColor: colors.content[150],
+        tabBarStyle: {
+          backgroundColor: colorScheme === "dark" ? colors.base[600] : colors.base[150],
+          borderTopColor: colors.primary.faded,
+        },
+        tabBarLabelPosition: "beside-icon",
+        tabBarHideOnKeyboard: true,
+      }}
+      sceneContainerStyle={{
+        backgroundColor: colorScheme === "dark" ? colors.base[600] : colors.base[150],
       }}
     >
       <Screen
         name="home"
         component={Home}
         options={{
-          title: "Billie",
+          title: "Lista",
           headerShown: false,
-          headerShadowVisible: false,
+          tabBarIcon: ({ color, size }) => <Icon name="list" color={color} size={size} />,
+        }}
+      />
+      <Screen
+        name="addTransaction"
+        component={AddTransaction}
+        options={{
+          title: "Nova",
+          headerTitle: "Adicionar transação",
+          tabBarIcon: ({ color, size }) => <Icon name="plus" color={color} size={size} />,
+        }}
+      />
+      <Screen
+        name="reports"
+        component={Reports}
+        options={{
+          title: "Resumo",
+          tabBarIcon: ({ color, size }) => <Icon name="pie-chart" color={color} size={size} />,
         }}
       />
     </Navigator>
