674736f6adc262e2b797d2b564c5a55ae176db44
Genes Luna <genesluna@gmail.com>
2023-05-17 13:02:05 -0300

    added auth provider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
-import { useColorScheme } from "react-native";
+import { SafeAreaView, useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Routes } from "./src/routes";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
   let colorScheme = useColorScheme();
 
   return (
-    <>
-      <Routes />
-      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-    </>
+    <AuthProvider>
+      <SafeAreaView className="flex-1">
+        <Routes />
+        <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+      </SafeAreaView>
+    </AuthProvider>
   );
 }
