2ad5d5ce01af14f910d3d8aafcfe2e11be6ffa6a
Genes Luna <genesluna@gmail.com>
2023-05-20 18:25:42 -0300

    added create user

--- src/screens/Auth/Register.tsx
+++ src/screens/Auth/Register.tsx
@@ -1,11 +1,11 @@
-import React from "react";
 import { Keyboard, KeyboardAvoidingView, Platform, ToastAndroid, TouchableWithoutFeedback } from "react-native";
 
-import { useAuth } from "../../context/AuthContext";
 import RegisterForm, { RegisterFormValues } from "../../components/forms/Auth/RegisterForm";
-import Header from "../../components/forms/Auth/Header";
+import { createUser } from "../../services/firestore/userService";
 import Container from "../../components/common/Container";
 import { useNavigation } from "@react-navigation/native";
+import Header from "../../components/forms/Auth/Header";
+import { useAuth } from "../../context/AuthContext";
 
 const Register = () => {
   const { register, reloadAuthUser } = useAuth();
@@ -17,6 +17,14 @@ const Register = () => {
       await result.user.updateProfile({ displayName: name.trim() });
       await result.user.sendEmailVerification();
       await reloadAuthUser();
+      await createUser(
+        {
+          name: result.user.displayName,
+          email: result.user.email,
+          photoURL: result.user.photoURL ?? "",
+        },
+        result.user.uid
+      );
       ToastAndroid.show("Usuário cadastrado com sucesso.", ToastAndroid.LONG);
       navigation.reset({ index: 0, routes: [{ name: "emailValidation" }] });
     } catch (error: any) {
