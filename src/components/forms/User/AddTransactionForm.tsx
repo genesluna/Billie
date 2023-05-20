453018dad94894d19789aa7fe9a019bd447cd76f
Genes Luna <genesluna@gmail.com>
2023-05-20 18:22:01 -0300

    added editing feature

--- src/components/forms/User/AddTransactionForm.tsx
+++ src/components/forms/User/AddTransactionForm.tsx
@@ -1,25 +1,32 @@
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { Keyboard, View, ViewProps, Modal, TextInput } from "react-native";
 import { Feather as Icon } from "@expo/vector-icons";
-import { useFormik } from "formik";
+import { useRoute } from "@react-navigation/native";
 import { useRef, useState } from "react";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 
+import { maskCurrency, onlyNumbersWithDecimal } from "../../../utils/textInputMasks";
+import { formatDate, getTransactionById } from "../../../utils/transactionsUtils";
+import { useTransactions } from "../../../context/TransactionsContext";
+import { Transaction } from "../../../models/Transaction";
 import { Category } from "../../../models/Category";
 import MaskedInput from "../../common/MaskedInput";
-import { formatDate } from "../../../utils/utils";
 import CategoriesList from "../../CategoriesList";
 import Button from "../../common/Button";
 import colors from "../../../../colors";
 import Input from "../../common/Input";
-import { Transaction } from "../../../models/Transaction";
-import { useAuth } from "../../../context/AuthContext";
 
 type AddTransactionFormProps = ViewProps & {
-  onSubmit: (trasaction: Transaction) => Promise<void>;
+  onAddTransaction: (trasaction: Transaction) => Promise<void>;
+  onUpdateTransaction: (trasaction: Transaction) => Promise<void>;
 };
 
-export type AddTransactionFormValues = {
+type ParamsProps = {
+  transactionId: string | undefined;
+};
+
+type AddTransactionFormValues = {
   description: string;
   amount: string;
   categoryName: string;
@@ -29,15 +36,20 @@ export type AddTransactionFormValues = {
 /**
  * A form for registering a new transaction.
  *
- * @param onSubmit - A callback function to be called when the form is submitted.
+ * @param onAddTrasaction - A callback function to be called when the form is submitted with a new transaction.
+ * @param onUpdateTrasaction - A callback function to be called when the form is submitted with a transaction to be updated.
  *
  * @returns - A component that contains the register form.
  */
-const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => {
-  const { authUser } = useAuth();
-  const [date, setDate] = useState<Date | undefined>(undefined);
-  const [type, setType] = useState<"income" | "expense">("expense");
-  const [category, setCategory] = useState<Category | undefined>(undefined);
+const AddTransactionForm = ({ onAddTransaction, onUpdateTransaction, ...props }: AddTransactionFormProps) => {
+  const route = useRoute();
+  const transactionId = (route.params as ParamsProps)?.transactionId ?? undefined;
+  const { transactions } = useTransactions();
+  const transaction = getTransactionById(transactions, transactionId ?? "");
+
+  const [date, setDate] = useState<Date | undefined>(transaction?.date ?? undefined);
+  const [type, setType] = useState<"income" | "expense" | string>(transaction?.type ?? "expense");
+  const [category, setCategory] = useState<Category | undefined>(transaction?.category ?? undefined);
   const [showCategoriesModal, setShowCategoriesModal] = useState<boolean>(false);
 
   const amountInput = useRef<TextInput>(null);
@@ -45,10 +57,10 @@ const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => 
   const dateInput = useRef<TextInput>(null);
 
   const initialValues: AddTransactionFormValues = {
-    description: "",
-    amount: "",
-    categoryName: "",
-    date: "",
+    description: transaction?.description ?? "",
+    amount: maskCurrency(transaction?.amount.toString() ?? ""),
+    categoryName: transaction?.category.name ?? "",
+    date: transaction?.date.toString() ?? "",
   };
 
   const addTtansactionSchema = Yup.object().shape({
@@ -62,16 +74,25 @@ const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => 
     validationSchema: addTtansactionSchema,
     initialValues: initialValues,
     onSubmit: async (values) => {
-      await onSubmit({
-        description: values.description,
-        amount: parseFloat(values.amount),
-        type: type,
-        category: category!,
-        date: date!,
-        userId: authUser?.uid!,
-      });
+      if (transactionId) {
+        await onUpdateTransaction({
+          Id: transactionId,
+          description: values.description,
+          amount: onlyNumbersWithDecimal(values.amount),
+          type: type,
+          category: category!,
+          date: date!,
+        });
+      } else {
+        await onAddTransaction({
+          description: values.description,
+          amount: onlyNumbersWithDecimal(values.amount),
+          type: type,
+          category: category!,
+          date: date!,
+        });
+      }
     },
-    validateOnChange: true,
   });
 
   function handleCategorySelection(category: Category) {
@@ -137,6 +158,7 @@ const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => 
         returnKeyType="next"
         onChangeText={handleChange("description")}
         onBlur={handleBlur("description")}
+        value={values.description}
         error={errors.description}
         touched={touched.description}
         onSubmitEditing={() => amountInput.current?.focus()}
@@ -150,8 +172,9 @@ const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => 
         returnKeyType="next"
         keyboardType="number-pad"
         keyboardAppearance="dark"
-        onChangeUnmasked={handleChange("amount")}
+        onChangeMask={handleChange("amount")}
         onBlur={handleBlur("amount")}
+        value={values.amount}
         error={errors.amount}
         touched={touched.amount}
       />
@@ -175,7 +198,7 @@ const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => 
       <Input
         ref={dateInput}
         icon="calendar"
-        placeholder="Escolha a data"
+        placeholder="Informe a data"
         showSoftInputOnFocus={false}
         onPressIn={() => {
           Keyboard.dismiss();
@@ -190,8 +213,8 @@ const AddTransactionForm = ({ onSubmit, ...props }: AddTransactionFormProps) => 
 
       <Button
         className="w-full mt-1"
-        icon="save"
-        label="salvar"
+        icon={transactionId ? "refresh-cw" : "save"}
+        label={transactionId ? "atualizar" : "salvar"}
         onPress={() => handleSubmit()}
         isLoading={isSubmitting}
       />
