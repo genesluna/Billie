4a44eec4d6c7d859ea2f8dc1e551ebe12639d1a1
Genes Luna <genesluna@gmail.com>
2023-05-20 18:31:58 -0300

    added sum by category

--- src/screens/user/Reports.tsx
+++ src/screens/user/Reports.tsx
@@ -1,9 +1,15 @@
+import { sumAmountsByCategory } from "../../utils/transactionsUtils";
+import { useTransactions } from "../../context/TransactionsContext";
 import Container from "../../components/common/Container";
 import Text from "../../components/common/Text";
 
-type Props = {};
+const Reports = () => {
+  const { transactions } = useTransactions();
 
-const Reports = (props: Props) => {
+  let amountSum = sumAmountsByCategory(transactions);
+
+  console.log(amountSum);
+
   return (
     <Container>
       <Text size="h2">Resumo</Text>
