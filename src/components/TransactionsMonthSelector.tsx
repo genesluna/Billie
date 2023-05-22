0c5a4acf8011a7e44cb25f7845427f5247f856ff
Genes Luna <genesluna@gmail.com>
2023-05-22 01:20:44 -0300

    now holds its entire logic

--- src/components/TransactionsMonthSelector.tsx
+++ src/components/TransactionsMonthSelector.tsx
@@ -1,32 +1,42 @@
-import { View, ViewProps, GestureResponderEvent } from "react-native";
+import { View, ViewProps } from "react-native";
 
+import { useTransactions } from "../context/TransactionsContext";
 import { months } from "../utils/transactionsUtils";
 import Button from "./common/Button";
 import Text from "./common/Text";
 
-type TransactionsMonthSelectorProps = ViewProps & {
-  initialDate: Date | undefined;
-  currentDate: Date;
-  onPreviousMonth: (event: GestureResponderEvent) => Promise<void>;
-  onNextMonth: (event: GestureResponderEvent) => Promise<void>;
-};
+type TransactionsMonthSelectorProps = ViewProps & {};
 
-const TransactionsMonthSelector = ({
-  initialDate,
-  currentDate,
-  onNextMonth,
-  onPreviousMonth,
-  ...props
-}: TransactionsMonthSelectorProps) => {
+const TransactionsMonthSelector = ({ ...props }: TransactionsMonthSelectorProps) => {
+  const { transactions, oldestTransactionDate, handlePreviousAndNextMonthTransactions } = useTransactions();
+  const currentDate = transactions[0]?.date ?? new Date();
+  const initialDate = oldestTransactionDate;
+
+  async function onPreviousMonth(): Promise<void> {
+    const currentMonth = transactions[0]?.date?.getMonth() ?? new Date().getMonth();
+    const currentYear = transactions[0]?.date?.getFullYear() ?? new Date().getFullYear();
+    const month = currentMonth > 0 ? currentMonth - 1 : 11;
+    const year = currentMonth !== 0 ? currentYear : currentYear - 1;
+    return await handlePreviousAndNextMonthTransactions(month, year);
+  }
+
+  async function onNextMonth(): Promise<void> {
+    const currentMonth = transactions[0]?.date?.getMonth() ?? new Date().getMonth();
+    const currentYear = transactions[0]?.date?.getFullYear() ?? new Date().getFullYear();
+    const month = currentMonth < 11 ? currentMonth + 1 : 0;
+    const year = currentMonth !== 11 ? currentYear : currentYear + 1;
+    return await handlePreviousAndNextMonthTransactions(month, year);
+  }
+
   if (!initialDate) return null;
 
   return (
-    <View className="flex-row items-center justify-between w-full pt-3 pb-5" {...props}>
+    <View className="flex-row items-center justify-between w-full px-2 pt-3 pb-5" {...props}>
       {initialDate.getMonth() !== currentDate.getMonth() ? (
         <Button
           icon="arrow-left"
           iconOnly
-          className="w-10 h-10 rounded-full bg-primary/50 dark:bg-primary-faded"
+          className="w-10 h-10 rounded-full bg-primary/50 dark:bg-primary/30"
           onPress={onPreviousMonth}
         />
       ) : (
@@ -39,7 +49,7 @@ const TransactionsMonthSelector = ({
         <Button
           icon="arrow-right"
           iconOnly
-          className="w-10 h-10 rounded-full bg-primary/50 dark:bg-primary-faded"
+          className="w-10 h-10 rounded-full bg-primary/50 dark:bg-primary/30"
           onPress={onNextMonth}
         />
       ) : (
