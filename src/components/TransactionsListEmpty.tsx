a157d2e83ea9903d7fff7a6a97839d842c3268bd
Genes Luna <genesluna@gmail.com>
2023-05-22 01:21:12 -0300

    now holds its entire logic

--- src/components/TransactionsListEmpty.tsx
+++ src/components/TransactionsListEmpty.tsx
@@ -1,29 +1,29 @@
 import { View, ViewProps, useColorScheme } from "react-native";
 import { Feather as Icon } from "@expo/vector-icons";
 
+import { useTransactions } from "../context/TransactionsContext";
 import colors from "../../colors";
 import Text from "./common/Text";
 
-type TransactionsListEmptyProps = ViewProps & {
-  initialDate: Date | undefined;
-};
+type TransactionsListEmptyProps = ViewProps & {};
 
-const TransactionsListEmpty = ({ initialDate, ...props }: TransactionsListEmptyProps) => {
+const TransactionsListEmpty = ({ ...props }: TransactionsListEmptyProps) => {
   let colorScheme = useColorScheme();
+  const { oldestTransactionDate } = useTransactions();
 
   return (
-    <View className="items-center px-5 pb-20 flex-1 justify-center">
+    <View className="items-center justify-center flex-1 px-5 pb-20" {...props}>
       <Icon name="inbox" size={76} color={colorScheme === "dark" ? colors.content[150] : colors.content[400]} />
-      {!initialDate ? (
-        <Text size="xl" className="mt-4 text-center font-medium">
+      {!oldestTransactionDate ? (
+        <Text size="xl" className="mt-4 font-medium text-center">
           Ainda não há transações
         </Text>
       ) : (
-        <Text size="xl" className="mt-4 text-center font-medium">
+        <Text size="xl" className="mt-4 font-medium text-center">
           Ainda não há transações esse mês
         </Text>
       )}
-      <Text size="base" className="text-center mt-4">
+      <Text size="base" className="mt-4 text-center">
         Clique no botão{" "}
         <Text size="base" className="font-bold text-primary-focus dark:text-primary">
           Adicionar
