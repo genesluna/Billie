8c12eee79b89d08ca2fee65ac44d325ba312e11d
Genes Luna <genesluna@gmail.com>
2023-06-04 20:10:50 -0300

    added modal and logic to update user's profile

--- src/screens/user/Home.tsx
+++ src/screens/user/Home.tsx
@@ -1,8 +1,11 @@
-import { View, FlatList, ActivityIndicator, Alert } from "react-native";
+import { View, FlatList, ActivityIndicator, Alert, Modal } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 
+import UserProfile, { UserProfileFormValues } from "../../components/forms/User/UserProfileForm";
 import TransactionsMonthSelector from "../../components/TransactionsMonthSelector";
 import TransactionsListEmpty from "../../components/TransactionsListEmpty";
+import { usersCollection } from "../../services/firestore/userService";
 import { useTransactions } from "../../context/TransactionsContext";
 import TransactionCard from "../../components/TransactionCard";
 import HighlightCards from "../../components/HighlightCards";
@@ -11,11 +14,14 @@ import { Transaction } from "../../models/Transaction";
 import HomeHeader from "../../components/HomeHeader";
 import { useAuth } from "../../context/AuthContext";
 import colors from "../../../colors";
+import { User } from "../../models/User";
 
 const Home = () => {
   const { transactions, isLoading, deleteItemFromCurrentMonthTransactions, deleteTransaction } = useTransactions();
+  const [showProfileModal, setShowProfileModal] = useState<boolean>(false);
+  const [appUser, setAppUser] = useState<User | undefined>(undefined);
   const navigation = useNavigation();
-  const { authUser } = useAuth();
+  const { authUser, updateUser } = useAuth();
 
   function handleEdit(item: Transaction) {
     navigation.navigate("addTransaction", { transactionId: item.Id! });
@@ -41,9 +47,54 @@ const Home = () => {
     ]);
   }
 
+  async function handleProfileSubmit(values: UserProfileFormValues) {
+    console.log(values);
+    try {
+      await updateUser(
+        {
+          email: values.email,
+          name: values.name,
+          phoneNumber: values.phoneNumber,
+          photoURL: values.photoURL,
+        },
+        authUser?.uid!
+      );
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setShowProfileModal(false);
+    }
+  }
+
+  /**
+   * Sets up an observer to listen for changes in the user's document state.
+   *
+   * @returns A function to unsubscribe the observer when the component unmounts.
+   */
+  useEffect(() => {
+    const subscriber = usersCollection.doc(authUser?.uid).onSnapshot(
+      (documentSnapshot) => {
+        if (documentSnapshot.exists) {
+          setAppUser(documentSnapshot.data());
+        }
+      },
+      (error) => {
+        if (appUser) setAppUser(undefined);
+        console.log(error);
+      }
+    );
+
+    return subscriber; // unsubscribe on unmount
+  }, [authUser?.uid]);
+
   return (
     <View className="flex-1">
-      <HomeHeader />
+      <HomeHeader
+        appUser={appUser ?? {}}
+        onProfilePress={() => {
+          setShowProfileModal(true);
+        }}
+      />
 
       <HighlightCards transactions={transactions} isLoading={isLoading} />
 
@@ -70,6 +121,15 @@ const Home = () => {
           <ActivityIndicator size={60} color={colors.primary.DEFAULT} />
         )}
       </Container>
+      <Modal animationType="slide" visible={showProfileModal}>
+        <UserProfile
+          appUser={appUser ?? {}}
+          onSubmit={handleProfileSubmit}
+          onClose={() => {
+            setShowProfileModal(false);
+          }}
+        />
+      </Modal>
     </View>
   );
 };
