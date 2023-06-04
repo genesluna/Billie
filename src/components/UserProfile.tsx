c0e7b45c005a3d6f7e69f907bbc0c58a7cecc4de
Genes Luna <genesluna@gmail.com>
2023-06-04 14:41:32 -0300

    added user profile skeleton

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+type Props = {};
+
+const UserProfile = (props: Props) => {
+  return (
+    <View>
+      <Text>UserProfile</Text>
+    </View>
+  );
+};
+
+export default UserProfile;
