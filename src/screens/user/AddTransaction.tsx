fbeb686abf426aac0235c6324f08d5991599a28f
Genes Luna <genesluna@gmail.com>
2023-06-07 11:20:36 -0300

    added transaction image upload logic

--- src/screens/user/AddTransaction.tsx
+++ src/screens/user/AddTransaction.tsx
@@ -1,8 +1,11 @@
 import { Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import storage from "@react-native-firebase/storage";
+import * as Crypto from "expo-crypto";
 
 import AddTransactionForm from "../../components/forms/User/AddTransactionForm";
 import { useTransactions } from "../../context/TransactionsContext";
+import { getTransactionById } from "../../utils/transactionsUtils";
 import Container from "../../components/common/Container";
 import { Transaction } from "../../models/Transaction";
 import { useAuth } from "../../context/AuthContext";
@@ -10,27 +13,59 @@ import { useAuth } from "../../context/AuthContext";
 const AddTransaction = () => {
   const navigation = useNavigation();
   const { authUser } = useAuth();
-  const { addTransaction, updateTransaction, addItemToCurrentMonthTransactions, updateItemInCurrentMonthTransactions } =
-    useTransactions();
+  const {
+    transactions,
+    addTransaction,
+    updateTransaction,
+    addItemToCurrentMonthTransactions,
+    updateItemInCurrentMonthTransactions,
+  } = useTransactions();
 
-  async function handleAddTransaction(trasaction: Transaction): Promise<void> {
+  async function handleAddTransaction(transaction: Transaction): Promise<void> {
     try {
-      const result = await addTransaction(trasaction, authUser?.uid!);
-      trasaction.Id = result.id;
-      addItemToCurrentMonthTransactions(trasaction);
+      if (transaction.photoURL) {
+        const path = `${authUser?.uid}/${transaction.date.getFullYear()}/${
+          transaction.date.getMonth() + 1
+        }/${Crypto.randomUUID()}.jpeg`;
+        transaction.photoURL = await handleImageUpload(transaction.photoURL, path);
+      }
+
+      const result = await addTransaction(transaction, authUser?.uid!);
+      transaction.Id = result.id;
+      addItemToCurrentMonthTransactions(transaction);
       navigation.navigate("home");
     } catch (error) {
       console.log(error);
     }
   }
 
-  async function handleUpdateTransaction(trasaction: Transaction): Promise<void> {
+  async function handleUpdateTransaction(transaction: Transaction): Promise<void> {
     try {
-      await updateTransaction(trasaction, authUser?.uid!);
-      updateItemInCurrentMonthTransactions(trasaction);
+      const oldTransaction = getTransactionById(transactions, transaction.Id!);
+
+      if (transaction.photoURL && transaction.photoURL !== oldTransaction?.photoURL) {
+        const path = `${authUser?.uid}/${transaction.date.getFullYear()}/${
+          transaction.date.getMonth() + 1
+        }/${Crypto.randomUUID()}.jpeg`;
+        transaction.photoURL = await handleImageUpload(transaction.photoURL, path);
+      }
+
+      await updateTransaction(transaction, authUser?.uid!);
+      updateItemInCurrentMonthTransactions(transaction);
       navigation.navigate("home");
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async function handleImageUpload(uri: string, path: string): Promise<string | undefined> {
+    try {
+      const reference = storage().ref(path);
+      await reference.putFile(uri);
+      return await reference.getDownloadURL();
+    } catch (error: any) {
+      console.log(error);
+      throw new Error(error);
     }
   }
 
